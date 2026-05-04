f88e21cdaa252b70d7eb4b21f70a8eebd9ef8c89
Datu Beru <berudatu@gmail.com>
2026-05-05 03:09:48 +0700

    Add protected routes for authenticated pages

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -6,19 +6,61 @@ import LogInjectionPage from "../pages/LogInjectionPage";
 import PreCheckPage from "../pages/PreCheckPage";
 import HistoryPage from "../pages/HistoryPage";
 import EditInjectionPage from "../pages/EditInjectionPage";
+import ProtectedRoute from "./ProtectedRoute";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/dashboard" replace />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
+
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/log-injection" element={<LogInjectionPage />} />
-        <Route path="/pre-check" element={<PreCheckPage />} />
-        <Route path="/history" element={<HistoryPage />} />
-	<Route path="/edit-injection/:id" element={<EditInjectionPage />} />
+
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <DashboardPage />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/log-injection"
+          element={
+            <ProtectedRoute>
+              <LogInjectionPage />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/pre-check"
+          element={
+            <ProtectedRoute>
+              <PreCheckPage />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/history"
+          element={
+            <ProtectedRoute>
+              <HistoryPage />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/edit-injection/:id"
+          element={
+            <ProtectedRoute>
+              <EditInjectionPage />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
